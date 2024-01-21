54475f7ccc4140669f774bc13931bcd749806ed3
Benenlux <simvee2000@gmail.com>
2024-01-21 12:51:13 +0100

    Minor changes to implement new Object class

---
+++ source/main.h
@@ -0,0 +1,35 @@
+#pragma once
+
+//Standard libraries
+#include <iostream>
+#include <fstream>
+#include <string>
+
+//Graphics libraries
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+//Math libraries
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+//Custom libraries
+#include "shader.h"
+
+
+class Main {
+public:
+	int Main::Init();
+
+private:
+	int window_width = 800;
+	int window_height = 600;
+	
+	void Main::Input(GLFWwindow* window);
+	int Main::Exit();
+	
+};
