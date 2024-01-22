0f783bdf81185a82cc2d9b7bed4870d1b58ec360
Benenlux <simvee2000@gmail.com>
2024-01-22 17:32:31 +0100

    Created the vertexbuffer class to make dynamic vertex buffers and bind and unbind them individually

---
+++ source/VertexBuffer.cpp
@@ -0,0 +1,22 @@
+#include "VertexBuffer.h"
+
+
+
+VertexBuffer::VertexBuffer(const void* data, unsigned int size) {
+	glGenBuffers(1, &m_RenderID);
+	glBindBuffer(GL_ARRAY_BUFFER, m_RenderID);
+	glBufferData(GL_ARRAY_BUFFER, size * sizeof(GLfloat), data, GL_STATIC_DRAW);
+
+}
+
+VertexBuffer::~VertexBuffer() {
+	glDeleteBuffers(1, &m_RenderID);
+}
+
+void VertexBuffer::Bind() const{
+	glBindBuffer(GL_ARRAY_BUFFER, m_RenderID);
+}
+
+void VertexBuffer::Unbind() const{
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
---
+++ source/VertexBuffer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <glad/glad.h>
+
+class VertexBuffer {
+private:
+	unsigned int m_RenderID;
+
+public:
+	VertexBuffer(const void* data, unsigned int size);
+	~VertexBuffer();
+
+	void Bind() const;
+	void Unbind() const;
+
+};
