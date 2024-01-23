95ff7eda77fe3212ed4722bffbcff62897b415f9
Benenlux <simvee2000@gmail.com>
2024-01-23 20:39:44 +0100

    Fleshed out the functions and members

--- source/graphics/Model.cpp
+++ source/graphics/Model.cpp
@@ -1,11 +1,25 @@
 #include "Model.h"
 
-Model::Model(std::vector<float> vertices, std::vector<unsigned int> indices){
-	
-	VertexBuffer vb(vertices.data(), vertices.size());
-	IndexBuffer ib(indices.data(), indices.size());
+Model::Model(std::vector<float> vertices, std::vector<unsigned int> indices, Shader shader, Properties properties){
+	m_shader = shader;
+	m_vertices = vertices;
+	m_indices = indices;
+	rb.properties = properties;
 }
+
+void Model::CreateVertexBuffer() {
+	vb.Create(m_vertices.data(), m_vertices.size());
+}
+
+void Model::CreateIndexBuffer() {
+	ib.Create(m_indices.data(), m_indices.size());
+}
+
+
 
 void Model::Render() {
 	
+
+	m_shader.BindUniform3f("translation", m_translation);
+	glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, 0);
 }
--- source/graphics/Model.h
+++ source/graphics/Model.h
@@ -3,17 +3,26 @@
 #include <vector>
 
 #include "../physics/Rigidbody.h"
+#include "../physics/Properties.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "Shader.h"
+#include "VertexArray.h"
 
-class Model
-{
+class Model {
 public:
 	RigidBody rb;
+	VertexBuffer vb;
+	IndexBuffer ib;
+	glm::vec3 m_translation = glm::vec3(0.0f, 0.0f, 0.0f);
+	float dt; 
 
-	Model(std::vector<float> vertices, std::vector<unsigned int> indices);
-	~Model();
+	Model(std::vector<float> vertices, std::vector<unsigned int> indices, Shader shader, Properties properties);
+	void CreateVertexBuffer();
+	void CreateIndexBuffer();
 	void Render();
-
 private:
+	std::vector<float> m_vertices;
+	std::vector<unsigned int> m_indices;
+	Shader m_shader;
 };
