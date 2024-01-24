7c0bc516d077c15d2e785980f96120d5acf73213
Benenlux <simvee2000@gmail.com>
2024-01-24 10:03:18 +0100

    Added a scale matrix

--- resources/VertexShader.glsl
+++ resources/VertexShader.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
-
-
+uniform vec3 translation;
+uniform mat4 scale;
 
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = scale * vec4(aPos.x + translation.x, aPos.y + translation.y, aPos.z + translation.z, 1.0);
 }
