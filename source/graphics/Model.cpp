9f725be499e0d15e117edf969b70d68b9fdf20ed
Benenlux <simvee2000@gmail.com>
2024-01-24 10:04:03 +0100

    Automatically binds buffers and renders with rigidbody behavior

--- source/graphics/Model.cpp
+++ source/graphics/Model.cpp
@@ -1,10 +1,14 @@
 #include "Model.h"
 
-Model::Model(std::vector<float> vertices, std::vector<unsigned int> indices, Shader shader, Properties properties){
+Model::Model(std::vector<float> vertices, std::vector<unsigned int> indices, Shader shader, VertexBufferLayout layout, int pointCount){
 	m_shader = shader;
 	m_vertices = vertices;
 	m_indices = indices;
-	rb.properties = properties;
+	va.Bind();
+	layout.Push<float>(3);
+	CreateVertexBuffer();
+	va.AddBuffer(vb, layout);
+	CreateIndexBuffer();
 }
 
 void Model::CreateVertexBuffer() {
@@ -18,8 +22,11 @@ void Model::CreateIndexBuffer() {
 
 
 void Model::Render() {
-	
-
+	va.Bind();
+	rb.Update();
+	glm::mat4 m_scale = glm::scale(glm::mat4(1.0f), glm::vec3(scale));
+	m_translation += rb.GetVelocity();
 	m_shader.BindUniform3f("translation", m_translation);
+	m_shader.BindMat4("scale", m_scale);
 	glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, 0);
 }
--- source/graphics/Model.h
+++ source/graphics/Model.h
@@ -2,22 +2,27 @@
 
 #include <vector>
 
-#include "../physics/Rigidbody.h"
-#include "../physics/Properties.h"
+
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
 #include "Shader.h"
 #include "VertexArray.h"
+#include "VertexBufferLayout.h"
+#include "../physics/Rigidbody.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 class Model {
 public:
 	RigidBody rb;
 	VertexBuffer vb;
+	VertexArray va;
 	IndexBuffer ib;
+	float scale = 1.0f;
 	glm::vec3 m_translation = glm::vec3(0.0f, 0.0f, 0.0f);
-	float dt; 
 
-	Model(std::vector<float> vertices, std::vector<unsigned int> indices, Shader shader, Properties properties);
+	Model(std::vector<float> vertices, std::vector<unsigned int> indices, Shader shader, VertexBufferLayout layout, int pointCount);
 	void CreateVertexBuffer();
 	void CreateIndexBuffer();
 	void Render();
