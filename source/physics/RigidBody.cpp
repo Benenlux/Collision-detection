93d71c8bd2202079156f1bef82b6a7e6053d39c8
Benenlux <simvee2000@gmail.com>
2024-01-29 21:22:48 +0100

    Started development on gravity translation

--- source/physics/RigidBody.cpp
+++ source/physics/RigidBody.cpp
@@ -2,12 +2,13 @@
 
 
 
-void RigidBody::Update() {
-	dt = glfwGetTime() - dt;
+glm::vec2 RigidBody::Update(float deltaTime) {
 
-	velocity.y -= 9.81 * dt;
+	force.y =+ -9.81f * mass;
 	
-
+	force * 0.0000001f;
+	velocity =+ force / mass;
+	return GetVelocity();
 }
 
 glm::vec3 RigidBody::GetVelocity() {
--- source/physics/Rigidbody.h
+++ source/physics/Rigidbody.h
@@ -11,13 +11,15 @@ public:
 	glm::vec3 velocity = glm::vec3(0.0f, 0.0f, 0.0f);
 	
 
-	void Update();
-	glm::vec3 RigidBody::GetVelocity();
+	glm::vec2 Update(float deltaTime);
+	
 
 	// TODO: Add a function which handles collission between this and the other rigidbody
 	// The funciton is called through the scene, which then passes the rigidbody from the other model
 	// The function will then apply the proper behavior based on this and the other models' rigidbody
 private:
-	glm::vec3 force = glm::vec3(0.0f, 0.0f, 0.0f);
 	
+	glm::vec3 RigidBody::GetVelocity();
+	glm::vec3 force = glm::vec3(0.0f, 0.0f, 0.0f);
+	float mass = 1;
 };
