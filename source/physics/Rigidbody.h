f3c3251cd9c78a519d1f4718e9e727e5d69494e1
Benenlux <simvee2000@gmail.com>
2024-02-11 19:57:07 +0100

    Cleaned up comments

--- source/physics/Rigidbody.h
+++ source/physics/Rigidbody.h
@@ -14,10 +14,6 @@ public:
 
 	glm::vec2 Update(float deltaTime);
 	
-
-	// TODO: Add a function which handles collission between this and the other rigidbody
-	// The funciton is called through the scene, which then passes the rigidbody from the other model
-	// The function will then apply the proper behavior based on this and the other models' rigidbody
 private:
 	
 	glm::vec3 RigidBody::GetVelocity();
