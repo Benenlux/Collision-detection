7c652850c4d59ed8675a3d668f42cc9d0ac4f96e
Benenlux <simvee2000@gmail.com>
2024-02-27 13:52:46 +0100

    Added scaling according to window size

--- resources/VertexShader.glsl
+++ resources/VertexShader.glsl
@@ -1,7 +1,9 @@
 #version 330 core
 layout (location = 0) in vec4 aPos;
 
+uniform float ratio;
+
 void main()
 {
-    gl_Position = vec4(aPos);
+    gl_Position = vec4(aPos.x/ratio, aPos.y, aPos.z ,1.0);
 }
