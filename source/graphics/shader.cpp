e4bd7898d67b0d9c29fd7fb040686219a50099ef
Benenlux <simvee2000@gmail.com>
2024-02-27 13:53:40 +0100

    Added function to bind floats to shaders

--- source/graphics/shader.cpp
+++ source/graphics/shader.cpp
@@ -69,13 +69,23 @@ unsigned int Shader::CreateFragShader(const std::string& path) {
 }  
 
 void Shader::BindUniform3f(std::string uniformName, glm::vec3 data) {
-	int vertexColorLocation = glGetUniformLocation(m_shaderProgram, uniformName.c_str());
-	glUniform3f(vertexColorLocation, data.x, data.y, data.z);
+	int uniformLocation = glGetUniformLocation(m_shaderProgram, uniformName.c_str());
+	glUniform3f(uniformLocation, data.x, data.y, data.z);
 }
 
 void Shader::BindMat4(std::string uniformName, glm::mat4 data) {
-	int vertexColorLocation = glGetUniformLocation(m_shaderProgram, uniformName.c_str());
-	glUniformMatrix4fv(vertexColorLocation, 1, GL_FALSE, glm::value_ptr(data));
+	int uniformLocation = glGetUniformLocation(m_shaderProgram, uniformName.c_str());
+	glUniformMatrix4fv(uniformLocation, 1, GL_FALSE, glm::value_ptr(data));
+}
+
+void Shader::BindUniform2f(std::string uniformName, glm::vec2 data) {
+	int uniformLocation = glGetUniformLocation(m_shaderProgram, uniformName.c_str());
+	glUniform3f(uniformLocation, data.x, data.y, 0);
+}
+
+void Shader::BindUniform1f(std::string uniformName, float data) {
+	int uniformLocation = glGetUniformLocation(m_shaderProgram, uniformName.c_str());
+	glUniform1f(uniformLocation, data);
 }
 
 std::string Shader::Load_Shader(const std::string& path) {
