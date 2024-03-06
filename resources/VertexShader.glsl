c766311c800dab4d5cf8366effcdd62477e1a81a
Benenlux <simvee2000@gmail.com>
2024-03-06 10:31:50 +0100

    Added a way to change the color of the model

--- resources/VertexShader.glsl
+++ resources/VertexShader.glsl
@@ -2,8 +2,13 @@
 layout (location = 0) in vec4 aPos;
 
 uniform float ratio;
+uniform vec4 color;
+
+out vec4 vertexColor;
+
 
 void main()
 {
     gl_Position = vec4(aPos.x/ratio, aPos.y, aPos.z ,1.0);
+    vertexColor = color;
 }
